4964bdb31f35da14607bb03d0513331be378afd9
Fritz Eisenbrand <fritz.eisenbrand@gmail.com>
2017-02-28 11:03:00 +0100

    some toys

---
+++ auto/algorithms.el
@@ -0,0 +1,32 @@
+(TeX-add-style-hook
+ "algorithms"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (LaTeX-add-labels
+    "cha:runn-time-analys"
+    "ex-a-1"
+    "ex-a-3"
+    "def:2"
+    "def:1"
+    "alg:1"
+    "exe:det"
+    "alg:ex2"
+    "sec:analys-gauss-elim"
+    "alg:3"
+    "fig:9"
+    "co:11"
+    "co:10"
+    "thr:9"
+    "sec:exercises"
+    "item:3")))
+
