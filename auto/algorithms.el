56f41db0c5b92dabea3737117ab256bb96eb8ed7
Fritz Eisenbrand <fritz.eisenbrand@gmail.com>
2017-03-01 14:04:33 +0100

    update

--- auto/algorithms.el
+++ auto/algorithms.el
@@ -2,16 +2,16 @@
  "algorithms"
  (lambda ()
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (LaTeX-add-labels
     "cha:runn-time-analys"
     "ex-a-1"
@@ -28,5 +28,13 @@
     "co:10"
     "thr:9"
     "sec:exercises"
-    "item:3")))
+    "item:3"
+    "sec:fast-matr-mult"
+    "eq:str:1"
+    "eq:str:3"
+    "eq:stra:4"
+    "eq:str:5"
+    "fig:str:1"
+    "thr:10"
+    "item:str:4")))
 
