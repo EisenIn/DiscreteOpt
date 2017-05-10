efd485e00f6aafbec4f22cd0b9e54cc92301f6de
Fritz Eisenbrand <fritz.eisenbrand@gmail.com>
2017-05-10 15:02:15 +0200

    Changed notes on ellipsoid method

---
+++ auto/ellipsoid.el
@@ -0,0 +1,77 @@
+(TeX-add-style-hook
+ "ellipsoid"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (LaTeX-add-labels
+    "el:cha:ellipsoid-method"
+    "el:ex:6"
+    "el:eq:13"
+    "el:ex:8"
+    "el:lem:12"
+    "el:fig:half-ball"
+    "el:eq:16"
+    "el:eq:15"
+    "el:eq:17"
+    "el:eq:18"
+    "el:eq:19"
+    "el:thr:19"
+    "el:thr:22"
+    "el:eq:20"
+    "el:thr:20"
+    "el:sec:method"
+    "el:alg:3"
+    "el:item:2"
+    "el:item:1"
+    "el:item:3"
+    "el:item:4"
+    "el:thr:21"
+    "el:eq:23"
+    "sec:deciding-feasibility"
+    "item:4"
+    "item:7"
+    "item:8"
+    "el:ex:11"
+    "el:thr:24"
+    "el:eq:42"
+    "thr:11"
+    "thr:12"
+    "exercise:3"
+    "exercise:1"
+    "exercise:2"
+    "el:sec:separation-problem"
+    "el:ex:10"
+    "el:eq:26"
+    "el:eq:25"
+    "el:ex:7"
+    "el:sec:how-start-when-1"
+    "el:item:7"
+    "el:item:8"
+    "el:lem:11"
+    "el:eq:10"
+    "el:eq:11"
+    "el:eq:24"
+    "el:eq:44"
+    "el:eq:27"
+    "el:eq:38"
+    "el:eq:39"
+    "el:eq:40"
+    "el:eq:41"
+    "el:thr:23"
+    "el:sec:bound-full-dimens"
+    "el:sec:boundedness"
+    "el:thr:25"
+    "el:sec:ellips-meth-optim"
+    "el:eq:43"
+    "el:ex:12"
+    "el:sec:numerical-issues")))
+
--- auto/lecture.el
+++ auto/lecture.el
@@ -6,16 +6,16 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("graphicx" "pdftex") ("fontenc" "T1") ("inputenc" "utf8") ("babel" "american")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "algorithms"
